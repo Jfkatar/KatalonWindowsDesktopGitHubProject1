6ac16230f6f6a1d147691364b931e855512c662c
pimozulunifu <pimozulunifu@account-26975.1514-1-105-0-0-onespace-eis-us-2.vela.com>
2023-08-07 18:36:04 -0400

    Commit desktop application

---
+++ Test Suites/Desktop Windows app Test Suite1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Desktop Windows app Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5bf22018-3dd3-4b51-8f37-0c99e8bc48b0</testSuiteGuid>
+   <testCaseLink>
+      <guid>2e43cf99-afde-45f8-adee-42dd47b68e61</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Windows Test Case1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
